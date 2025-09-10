4eacb741c2b29f2f85dbb7337aa2ab4e24ef354e
Aster Ryan <aster@aster.fyi>
2025-09-10 07:20:50 -0700

    fix linkfiles
    i gotta fix this for real

--- link-files.sh
+++ link-files.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-ln -Fsv $(git ls-tree --name-only HEAD | sed "/^$(basename $0)$/d;/^\.gitmodules$/d" | awk -v P=$(pwd) '{print P"\/" $0}') ~
+ln -Fsv $(git ls-tree --name-only HEAD | sed "/^$(basename $0)$/d;/^\.gitmodules$/d" | awk -v P=$(pwd) '{print P"\/" $0}') ~/
